5707379c934e96b26630d703b31f36eac8598592
Jake <820715+jakelogemann@users.noreply.github.com>
2022-10-12 12:15:44 -0400

    moar things

--- main.go
+++ main.go
@@ -7,29 +7,39 @@ import (
 	x "github.com/caddyserver/xcaddy"
 )
 
+var OutputFile = "./x"
+
+var builder = x.Builder{
+	Compile: x.Compile{
+		Cgo: false,
+	},
+
+	CaddyVersion: "master",
+	Plugins: []x.Dependency{
+		{
+			PackagePath: "github.com/tailscale/caddy-tailscale",
+			Version:     "main",
+		},
+		{
+			PackagePath: "github.com/mholt/caddy-ratelimit",
+			Version:     "master",
+		},
+		{
+			PackagePath: "github.com/abiosoft/caddy-json-schema",
+			Version:     "master",
+		},
+	},
+	Replacements: []x.Replace{},
+	RaceDetector: false,
+	SkipCleanup:  false,
+	SkipBuild:    false,
+	Debug:        false,
+	BuildFlags:   "-ldflags '-w -s' -trimpath",
+	ModFlags:     "",
+}
+
 func main() {
-	builder := x.Builder{
-		Compile: x.Compile{
-			Cgo: false,
-		},
-
-		CaddyVersion: "master",
-		Plugins: []x.Dependency{
-			{
-				PackagePath: "github.com/tailscale/caddy-tailscale",
-				Version:     "main",
-			},
-		},
-		Replacements: []x.Replace{},
-		RaceDetector: false,
-		SkipCleanup:  false,
-		SkipBuild:    false,
-		Debug:        false,
-		BuildFlags:   "",
-		ModFlags:     "",
-	}
-
-	if err := builder.Build(context.Background(), "./x"); err != nil {
+	if err := builder.Build(context.Background(), OutputFile); err != nil {
 		log.Fatal(err)
 	}
 }
